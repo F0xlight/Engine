6de9497b24306c3f89165bad2742f5a7fc390412
Andéol _ <ande91@outlook.com>
2021-01-16 14:29:41 +0100

    first commit

---
+++ Engine/Source.cpp
@@ -0,0 +1,146 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <chrono>
+
+//adapt viewport size to window size
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+//handle inputs
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+//Vertex Shader
+const char *vertexShaderSource = "#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader
+const char *fragmentShaderSource = "#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main(){\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+int main(){
+
+	//intialize and configure glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4.6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//create a window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "WoW", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "GLFW did not initialize" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//initialize glad (load opengl function pointers)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "GLAD did not initialize" << std::endl;
+		return -1;
+	}
+	//Shader shit
+		//Vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	int success; //compile success ?
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+		//Fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	int fsuccess; //compile success ?
+	char infoLogf[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fsuccess);
+	if (!fsuccess) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLogf);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLogf << std::endl;
+	}
+		//Shader program
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	int psuccess; //linking success?
+	char infoLogp[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &psuccess);
+	if (!psuccess) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLogp);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLogp << std::endl;
+	}
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader); //remove shaders from memory
+	glDeleteShader(fragmentShader);
+	
+
+	//create triangle
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+
+
+	//create a VAO and bind it
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	//Vertex Buffer Objects  --> generate buffer ID (VBO)
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //set vertex attributes pointers
+	glEnableVertexAttribArray(0);
+
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//start chrono for fps
+		std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now(); 
+
+		//inputs
+		processInput(window); 
+		
+		//render body
+			//background color
+		glClearColor(0.1f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		
+		//smthing (drawing?)
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//events and buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		//end chrono and output fps
+		std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now(); 
+		int elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		std::cout << 1000000/elapsed_time << std::endl;
+	}
+
+	glfwTerminate();
+	return 0;
+}
